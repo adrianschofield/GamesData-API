d4b41f84f33f826a37e8f1eb892c4d5e3e98c69a
Adrian Schofield <adrianschofield@hotmail.co.uk>
2017-05-07 19:01:58 +0100

    Add postgres for heroku

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
+var env = process.env.NODE_ENV || 'development';
+var sequelize; 
+
+if (env === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
     'storage': __dirname + '/data/gamesdata-api.sqlite'
-});
+    });
+}
+
+
 
 var db = {};
 
